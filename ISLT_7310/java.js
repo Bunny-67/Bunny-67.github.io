3fcd918b5f69e81b0729375a993a4a216ca756df
Bunny-67 <143139694+Bunny-67@users.noreply.github.com>
2023-10-07 20:04:00 -0500

    Add files via upload

--- ISLT_7310/java.js
+++ ISLT_7310/java.js
@@ -1,3 +1,26 @@
-function toggleMobileMenu(menu) {
-    menu.classList.toggle('open');
-}
+
+
+
+function toggleMobileMenu() {
+    const mobileMenu = document.querySelector('.mobile-menu');
+    mobileMenu.classList.toggle('open');
+  }
+  
+  function toggleMobileSubMenu() {
+    const mobileSubMenu = document.querySelector('.mobile-sub-menu');
+    mobileSubMenu.classList.toggle('mopen');
+  }
+  
+  document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('#hamburger-icon').addEventListener('click', function () {
+        console.log('#Hamburger Clicked');
+        toggleMobileMenu();
+    });
+  
+    document.querySelector('#mobile-sub-menu-btn').addEventListener('click', function (event) {
+      event.stopPropagation(); 
+      toggleMobileSubMenu();
+    });
+
+  });
+  
